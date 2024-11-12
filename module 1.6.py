d81825aa773396d31cf7df6077b733a371649b1e
slike29 <craftcubs123@gmail.com>
2024-11-13 01:23:59 +1000

    Add files via upload

---
+++ module 1.6.py
@@ -0,0 +1,5 @@
+completed_dz = 12
+hours_spent = 1.5
+Course_name = "Python"
+time_for_the_task = hours_spent / completed_dz
+print(Course_name,"всего задач:",completed_dz,"затрачено часов:",hours_spent,"среднее время выполнение:",time_for_the_task,"часы.")
